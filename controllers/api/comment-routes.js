a65217c0f8cb03e0baf3861119cbb65ca61ea51a
nicoledodge <nicoledodge5@gmail.com>
2021-11-11 21:18:21 -0600

    added comment and post routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -26,6 +26,24 @@ router.put('/:id', async (req, res) => {
     }catch(error){
         res.status(500).json(error);
     }
-})
+});
+router.delete('/:id', async (req, res) => {
+    try{
+        console.log("Route working");
+
+        const deletedComment = await Comment.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        if(!deletedComment) {
+
+            res.status(404).json("No comment to delete");
+        }
+        res.status(200).json(deletedComment);
+    }catch(error) {
+        res.status(500).json(error);
+    }
+});
 
 module.exports = router;
---
+++ controllers/api/post-routes.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try{
+        const newPost = await Post.create({
+            title: req.body.title,
+            content: req.body.content,
+            user_id: req.session.user_id
+        });
+        res.status(200).json(newPost);
+    }
+    catch(error) {
+        res.status(500).json(error);
+    }
+
+});
+router.put('/:id', async (req, res) => {
+    try{
+        console.log('post put route');
+        console.log(req.body);
+        const updatedPost = await Post.update({title: req.body.title, content: req.body.content}, {
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json(updatedPost);
+    }catch(error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+});
+router.delete('/:id', async (req, res) => {
+    try{
+        const deletedPost = await Post.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        res.status(200).json(deletedPost);
+    }catch(error) {
+        res.status(500).json(error);
+    }
+});
+
+module.exports=router;
