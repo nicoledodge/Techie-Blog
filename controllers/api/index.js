795a7bae38ba4d74790a283d2cd462d711b4ba2f
nicoledodge <nicoledodge5@gmail.com>
2021-11-11 21:06:44 -0600

    finished homepage routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
-const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
+const userRoutes = require('./user-routes');
 const commentRoutes = require('./comment-routes');
 
-router.use('/user', userRoutes);
+
 router.use('/post', postRoutes);
+
+router.use('/user', userRoutes);
+
 router.use('/comment', commentRoutes);
 
 module.exports = router;
--- controllers/homepage.js
+++ controllers/homepage.js
@@ -1,1 +1,95 @@
 //get routes
+const router = require('express').Router();
+const {
+    User,
+    Post,
+    Comment
+} = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/dashboard', withAuth, async (req, res) => {
+    const postData = await Post.findAll({
+        include: [{
+            model: User,
+            required: true,
+            where: {
+                id: req.session.user_id
+            }
+        }]
+    });
+    const posts = postData.map((post) => {
+        return post.get({
+            plain: true
+        });
+    });
+    res.render('dashboard', {
+        posts,
+        logged_in: req.session.logged_in,
+        user_id: req.session.user_id
+    });
+});
+
+router.get('/', async (req, res) => {
+    const postData = await Post.findAll({
+        include: [{
+            model: User,
+            attributes: ['username']
+        }]
+    });
+    // console.log(postData.user);
+    const posts = postData.map((post) => {
+        return post.get({
+            plain: true
+        });
+    });
+    res.render('homepage', {
+        posts,
+        logged_in: req.session.logged_in,
+        userId: req.session.userId
+    });
+});
+router.get('/post/:id', withAuth, async (req, res) => {
+    const postData = await Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: User,
+                attributes: {
+                    exclude: 'password'
+                },
+
+            }
+        ]
+    });
+    const commentData = await Comment.findAll({
+        where: {
+            post_id: post.id
+        },
+        include: [{
+            model: User,
+            attributes: {
+                exclude: 'password'
+            }
+        }]
+    });
+    res.render('post', {
+        post,
+        comments,
+        logged_in: req.session.logged_in
+    });
+});
+
+router.get('/login', (req, res) => {
+    console.log(req.session.logged_in);
+    if (req.session.logged_in) {
+        res.redirect('/home');
+    }
+    res.render('login');
+});
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+module.exports = router;
